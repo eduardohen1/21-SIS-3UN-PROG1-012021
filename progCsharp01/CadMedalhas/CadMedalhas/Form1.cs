d4b188a0bf425684b210da1cd9f0b39caa8eb37d
Eduardo Henrique Marques Ferreira <eduardohmferreira@gmail.com>
2021-08-03 22:52:00 -0300

    Aula dia 03/08/2021 Aplicação C#.

---
+++ progCsharp01/CadMedalhas/CadMedalhas/Form1.cs
@@ -0,0 +1,47 @@
+﻿using CadMedalhas.controller;
+using CadMedalhas.model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadMedalhas
+{
+    public partial class Form1 : Form
+    {
+        //Atributo
+        DadosConexao dadosConexao = new DadosConexao("localhost",
+                                                     "root",
+                                                     "123456",
+                                                     "cadmedalhas");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = dadosConexao.ToString();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            ConexaoBD conexaoBD = new ConexaoBD(dadosConexao);
+            if (conexaoBD.conectar())
+                MessageBox.Show("Conectou no BD!!");
+            else
+                MessageBox.Show("Não conectou :(");
+        }
+    }
+}
